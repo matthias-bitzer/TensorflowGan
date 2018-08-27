555cd08079644ad4a400ac5251becb3ab64d18db
Matthias Bitzer <matthiasbitzer94@gmail.com>
2018-08-27 17:04:03 +0200

    Defined main functions

---
+++ gan_model.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+import numpy as np
+
+
+def generator_graph():
+    pass
+
+def discriminator_graph():
+    pass
+
+def building_whole_graph():
+    pass
+
+def train_models():
+    pass
+
+
+def load_models():
+    pass
+
+def generate_sample():
+    pass
+
+if __name__ == '__main__':
+    pass
+
+
+
+

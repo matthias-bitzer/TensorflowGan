1690437adb654edfcdf6e66ddd8f9ee0f1d75e5b
Matthias Bitzer <matthiasbitzer94@gmail.com>
2018-09-08 11:04:08 +0200

    changed to cgan

--- data_Processor.py
+++ data_Processor.py
@@ -13,22 +13,36 @@ class DataProcessor():
         from tensorflow.examples.tutorials.mnist import input_data
         mnist = input_data.read_data_sets('MNIST_data/',one_hot=True)
         self.data = mnist.train.images
+        self.labels = mnist.train.labels
         self.size = np.shape(self.data)[0]
+
+    def returnArtLabel(self):
+        randVar = (np.random.rand(1)*10).astype(np.int8)
+
+        vec = np.zeros(10)
+        vec[randVar]=1
+
+        return vec
 
     def nextBatch(self,batch_size):
         realImages = self.data[self.counter:self.counter+batch_size,:]
+        reallabels = self.labels[self.counter:self.counter+batch_size,:]
         shape = np.shape(realImages)
+        noiseLabel = np.array([self.returnArtLabel() for i in range(batch_size)])
+
         noisePriorBatch = np.random.randn(shape[0]*shape[1]).reshape(shape)
-        if self.counter + batch_size + 1 <= self.size:
+
+        if self.counter + batch_size + 1 <= (self.size-batch_size):
             self.counter = self.counter + batch_size + 1
         else:
             self.counter=0
 
-        return realImages,noisePriorBatch
+
+        return realImages,noisePriorBatch,reallabels,noiseLabel
 
     def nextBatchNoise(self,batch_size):
         shape = (batch_size,np.shape(self.data)[1])
-        noisePriorBatch = np.random.rand(shape[0] * shape[1]).reshape(shape)
+        noisePriorBatch = np.random.randn(shape[0] * shape[1]).reshape(shape)*10
         return noisePriorBatch
 
 
--- gan_model.py
+++ gan_model.py
@@ -6,64 +6,105 @@ import matplotlib.pyplot as plt
 
 sess = tf.Session()
 
-def generator_graph(input_dim,hidden_dims):
+def generator_graph(input_dim,hidden_dims,label_dim):
     x = tf.placeholder(tf.float32,[None,input_dim])
+    label = tf.placeholder(tf.float32,[None,label_dim])
+    input = tf.concat([x,label],axis=1)
+    weigth_init = 0.05
 
     with tf.variable_scope('generator_variables'):
-        W1 = tf.Variable(tf.random_uniform([input_dim, hidden_dims[0]], minval=-0.1, maxval=0.1),name='w1')
-        b1 = tf.Variable(tf.random_uniform([hidden_dims[0]],minval=-0.1,maxval=0.1),name= 'b1')
-        W2 = tf.Variable(tf.random_uniform([hidden_dims[0], hidden_dims[1]], minval=-0.1, maxval=0.1), name='w2')
-        b2 = tf.Variable(tf.random_uniform([hidden_dims[1]],minval=-0.1,maxval=0.1),name= 'b2')
-        W3 = tf.Variable(tf.random_uniform([hidden_dims[1], input_dim], minval=-0.1, maxval=0.1), name='w3')
-        b3 = tf.Variable(tf.random_uniform([input_dim],minval=-0.1,maxval=0.1),name= 'b3')
+        W1 = tf.Variable(tf.random_uniform([input_dim+label_dim, hidden_dims[0]], minval=-weigth_init, maxval=weigth_init),name='w1')
+        b1 = tf.Variable(tf.random_uniform([hidden_dims[0]],minval=-weigth_init,maxval=weigth_init),name= 'b1')
+        W2 = tf.Variable(tf.random_uniform([hidden_dims[0], hidden_dims[1]], minval=-weigth_init, maxval=weigth_init), name='w2')
+        b2 = tf.Variable(tf.random_uniform([hidden_dims[1]],minval=-weigth_init,maxval=weigth_init),name= 'b2')
+        W3 = tf.Variable(tf.random_uniform([hidden_dims[1], input_dim], minval=-weigth_init, maxval=weigth_init), name='w3')
+        b3 = tf.Variable(tf.random_uniform([input_dim],minval=-weigth_init,maxval=weigth_init),name= 'b3')
+        tf.summary.histogram('W1',W1)
+        tf.summary.histogram('W2', W2)
+        tf.summary.histogram('W3', W3)
+        tf.summary.histogram('b1', b1)
+        tf.summary.histogram('b2', b2)
+        tf.summary.histogram('b3', b3)
 
-        o1 = tf.nn.relu(tf.add(tf.matmul(x,W1),b1))
+
+        o1 = tf.nn.relu(tf.add(tf.matmul(input,W1),b1))
         o2 = tf.nn.relu(tf.add(tf.matmul(o1, W2), b2))
         generated = tf.add(tf.matmul(o2, W3), b3)
 
-    return generated,x
+    return generated,x,label
 
-def create_discriminator_variables(input_dim,hidden_dims):
-
+def create_discriminator_variables(input_dim,hidden_dims,label_dim):
+    weigth_init = 0.01
     with tf.variable_scope('discriminator_variables',reuse=True):
-        W1 = tf.Variable(tf.random_uniform([input_dim, hidden_dims[0]], minval=-0.1, maxval=0.1), name='w1')
-        b1 = tf.Variable(tf.random_uniform([hidden_dims[0]], minval=-0.1, maxval=0.1), name='b1')
-        W2 = tf.Variable(tf.random_uniform([hidden_dims[0], hidden_dims[1]], minval=-0.1, maxval=0.1), name='w2')
-        b2 = tf.Variable(tf.random_uniform([hidden_dims[1]], minval=-0.1, maxval=0.1), name='b2')
-        W3 = tf.Variable(tf.random_uniform([hidden_dims[1], 1], minval=-0.1, maxval=0.1), name='w3')
-        b3 = tf.Variable(tf.random_uniform([1], minval=-0.1, maxval=0.1), name='b3')
+        W1 = tf.Variable(tf.random_uniform([input_dim+label_dim, hidden_dims[0]], minval=-weigth_init, maxval=weigth_init), name='w1')
+        b1 = tf.Variable(tf.random_uniform([hidden_dims[0]], minval=-weigth_init, maxval=weigth_init), name='b1')
+        W2 = tf.Variable(tf.random_uniform([hidden_dims[0], hidden_dims[1]], minval=-weigth_init, maxval=weigth_init), name='w2')
+        b2 = tf.Variable(tf.random_uniform([hidden_dims[1]], minval=-weigth_init, maxval=weigth_init), name='b2')
+        W3 = tf.Variable(tf.random_uniform([hidden_dims[1], 1], minval=-weigth_init, maxval=weigth_init), name='w3')
+        b3 = tf.Variable(tf.random_uniform([hidden_dims[2]], minval=-weigth_init, maxval=weigth_init), name='b3')
+        W4 = tf.Variable(tf.random_uniform([hidden_dims[2], 1], minval=-weigth_init, maxval=weigth_init), name='w4')
+        b4 = tf.Variable(tf.random_uniform([1], minval=-weigth_init, maxval=weigth_init), name='b4')
+        tf.summary.histogram('W1',W1)
+        tf.summary.histogram('W2', W2)
+        tf.summary.histogram('W3', W3)
+        tf.summary.histogram('W4', W4)
+        tf.summary.histogram('b1', b1)
+        tf.summary.histogram('b2', b2)
+        tf.summary.histogram('b3', b3)
+        tf.summary.histogram('b4', b4)
 
-    return W1,W2,W3,b1,b2,b3
+    return W1,W2,W3,W4,b1,b2,b3,b4
 
 
 
-def discriminator_graph(input,variables):
-    W1, W2, W3, b1, b2, b3 = variables
-    o1 = tf.nn.relu(tf.add(tf.matmul(input, W1), b1))
+def discriminator_graph(input,variables,label):
+    W1, W2, W3,W4, b1, b2, b3,b4 = variables
+    input1 = tf.concat([input,label],axis=1)
+    #o1 = tf.nn.relu(tf.add(tf.matmul(input, W1), b1))
+    o1 = tf.nn.relu(tf.add(tf.matmul(input1, W1), b1))
     o2 = tf.nn.relu(tf.add(tf.matmul(o1, W2), b2))
-    likli = tf.sigmoid(tf.add(tf.matmul(o2, W3), b3))
+    o3 = tf.add(tf.matmul(o2, W3), b3)
+    likli = tf.sigmoid(tf.add(tf.matmul(o3, W4), b4))
+    #tf.summary.scalar('liklihood',likli)
 
-    return likli
+    return likli,o2
+
+def minibatch_discrimination(features):
+    pass
+
 
 
 def building_whole_graph(input_dim,hidden_dim_generator,hidden_dim_discriminator):
     real_pic = tf.placeholder(tf.float32,[None,input_dim])
-    generated, x = generator_graph(input_dim,hidden_dim_generator)
-    discriminator_variables = create_discriminator_variables(input_dim,hidden_dim_discriminator)
-    likli_false = discriminator_graph(generated,discriminator_variables)
-    likli_correct = discriminator_graph(real_pic,discriminator_variables)
+    real_label = tf.placeholder(tf.float32,[None,10])
+    generated, x, fake_label = generator_graph(input_dim,hidden_dim_generator,10)
+    g_image = tf.reshape(generated,[-1,28,28,1])
+    tf.summary.image('generated_image',g_image)
+    discriminator_variables = create_discriminator_variables(input_dim,hidden_dim_discriminator,10)
+    likli_false,o2_false = discriminator_graph(generated,discriminator_variables,fake_label)
+    likli_correct,o2_correct = discriminator_graph(real_pic,discriminator_variables,real_label)
     cost1 = -1*tf.reduce_mean(tf.log(likli_correct)+tf.log(1-likli_false))
-    cost2 = -1*tf.reduce_mean(tf.log(likli_false))
+    cost2 = -1*tf.reduce_mean(tf.log(likli_false))+tf.sqrt(tf.reduce_sum(tf.pow(o2_false-o2_correct,2)))
+    cost3 = tf.reduce_mean(tf.log(1-likli_false))
+    #tf.summary.scalar('likli_false',likli_false)
+    #tf.summary.scalar('likli_correct',likli_correct)
+    tf.summary.scalar('dis_cost',cost1)
+    tf.summary.scalar('gen_cost',cost2)
     generator_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,scope='generator_variables')
     printVarNames(generator_variables)
     discriminator_variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,scope='discriminator_variables')
     printVarNames(discriminator_variables)
-    optimizer1 = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(cost1,var_list=discriminator_variables)
-    optimizer2 =  tf.train.AdamOptimizer(learning_rate=1e-4).minimize(cost2,var_list=generator_variables)
+    optimizer1 = tf.train.AdamOptimizer(learning_rate=5e-5).minimize(cost1,var_list=discriminator_variables)
+    optimizer2 =  tf.train.AdamOptimizer(learning_rate=5e-5).minimize(cost2,var_list=generator_variables)
+    optimizer3 = tf.train.AdamOptimizer(learning_rate=5e-5).minimize(cost3, var_list=generator_variables)
+    merged = tf.summary.merge_all()
+    train_writer = tf.summary.FileWriter(path, sess.graph)
     init = tf.global_variables_initializer()
     sess.run(init)
+
     saver = tf.train.Saver()
-    return real_pic,x,generated,cost1,cost2,optimizer1,optimizer2,saver
+
+    return real_pic,x,generated,cost1,cost2,cost3,optimizer1,optimizer2,optimizer3,saver,real_label,fake_label,merged,train_writer
 
 def printVarNames(varlist):
     print("VarNames:")
@@ -71,41 +112,37 @@ def printVarNames(varlist):
 
         print(var.name)
 
-def train_models(models,batch_size,epochs,k):
-    real_pic, x, generated, cost1, cost2, optimizer1, optimizer2, saver = models
+def train_models(models,batch_size,epochs,k,switch_num):
+    real_pic, x, generated, cost1, cost2,cost3, optimizer1, optimizer2,optimizer3, saver,real_label_t,fake_label_t,merged,train_writer = models
+    first1 = True
+    counter_summa= 0
     for epoch in range(epochs):
         counter = 0
-        show_pic = True
+        show_pic = False
         epoch_loss1 = 0
         epoch_loss2 = 0
         first = True
         fig = plt.gcf()
 
 
+
         for i in range(int(train_size / batch_size)):
-            real_Image, noise = data.nextBatch(batch_size)
-            _, c1 = sess.run([optimizer1, cost1], feed_dict={real_pic: real_Image, x: noise})
+            real_Image, noise, real_label, noise_label = data.nextBatch(batch_size)
+            _, c1,summa = sess.run([optimizer1, cost1,merged], feed_dict={real_pic: real_Image, x: noise,real_label_t : real_label, fake_label_t : noise_label})
+            train_writer.add_summary(summa,counter_summa)
+            counter_summa +=1
             epoch_loss1 += c1
-            if show_pic:
-                show_pic=False
-                pic = sess.run([generated], feed_dict={x: noise})
-
-                pic = np.array(pic)
-                false_pic_to_show = pic[0,0,:].reshape(28, 28)
-                plt.imshow(false_pic_to_show,cmap='Greys')
-
-                plt.show(block=False)
-                plt.pause(0.01)
-                plt.close()
-
-
 
             if i % k == 0:
+                if True:
+                    #noise = data.nextBatchNoise(batch_size)
+                    _, c2 = sess.run([optimizer2, cost2], feed_dict={real_pic: real_Image, x: noise, real_label_t : real_label, fake_label_t : noise_label})
+                    epoch_loss2 += c2
 
-                noise = data.nextBatchNoise(batch_size)
-                _, c2 = sess.run([optimizer2, cost2], feed_dict={x: noise})
-                epoch_loss2 += c2
-        print("Epoche "+str(epoch)+" Loss1: "+str(epoch_loss1)+" Loss2: "+str(epoch_loss2/((int(train_size / batch_size)/k))))
+
+
+
+        print("Epoche "+str(epoch)+" D-Loss: "+str(epoch_loss1)+" G-Loss: "+str(epoch_loss2/((int(train_size / batch_size)/k))))
 
 
 
@@ -121,11 +158,12 @@ def generate_sample():
 
 if __name__ == '__main__':
 
+    path = 'tensorboard/train'
     data = data_Processor.DataProcessor()
     data.loadData()
     train_size = data.size
-    models = building_whole_graph(784,[200,200],[100,10])
-    train_models(models,32,100,1)
+    models = building_whole_graph(784,[50,40],[50,15,2])
+    train_models(models,64,500,3,40)
 
 
 
